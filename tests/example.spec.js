e37c51dbcf3b55997b63fe20c0d568d83bb2ee70
akash <akashb@heaptrace.com>
2025-09-18 17:54:30 +0530

    Added more test cases

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,22 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+// @ts-check
+test('homepage has Playwright in title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link navigates to intro page', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page).toHaveURL(/.*docs\/intro/);
+});
+
+test('search box should be visible', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const searchBox = page.getByRole('textbox', { name: 'Search' });
+  await expect(searchBox).toBeVisible();
+});
